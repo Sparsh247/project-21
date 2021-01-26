5e72054c8001bff9396aff9e9023a6193a372daf
Sparsh247 <74964540+Sparsh247@users.noreply.github.com>
2021-01-26 17:53:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,57 +6,55 @@ var speed,weight,thickness;
 
 
 function setup() {
-  createCanvas(1600,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(2000,1000);
+  bullet=createSprite(700, 400, 250, 40);
+bullet.shapeColor="orange"
+  
 thickness=random(22,83);
+
+wall=createSprite(1200,350,thickness,500);
+wall.shapeColor="yellow"
 speed=random(223,321);
 weight=random(30,52);
-bullet=createSprite(50,200,40,10);
-bullet.velocityX=speed;
 
 
-wall=createSprite(1200,200,thickness,height/2);
-wall.shapeColor=color(80,80,80)
+
+
+
 }
 
 function draw() {
-  background(255,255,255);  
+
+ background(7,8,54)
  
- if(wall.x-bullet,x <(bullet.width+wall.width)/2){
+ bullet.velocityX=speed;
 
-bullet.velocityX=0;
-var deformation=0.5 * weight * speed * speed/22509;
-if(deformation>180){
-
-  bullet.shapeColor=color(255,0,0)
-}
-if (deformation<180 && deformation>100)
+if(hasCollided(bullet,wall))
 {
+   bullet.velocityX=0;
+var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
 
-  bullet.shapeColor=color(230,230,0)
+if(damage>10)
+{
+  wall.shapeColor="red";
 }
-if(deformation<100){
-
-
-  bullet.shapeColor=color(0,255,0)
+if(damage<10)
+{
+  wall.shapeColor="green";
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
- }
- 
- 
- 
+}
  
   drawSprites();
+}
+
+function hasCollided(bullet,wall)
+{
+  bulletRightEdge=bullet.x +bullet.width;
+  wallLeftEdge= wall.x;
+  if (bulletRightEdge>=wallLeftEdge)
+  {
+    return true
+  }
+  return false;
 }
